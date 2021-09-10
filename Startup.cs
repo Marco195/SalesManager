be5a59f3f13e5dc8f3e03959242dd81f1e1355cc
Marco Aurélio Pavini Mondini <marco-aurelio.pm@hotmail.com>
2021-09-09 21:18:05 -0300

    Adicionada a primeira Migration.

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using SalesManager.Data;
 
 namespace SalesManager
 {
+
+    /// <summary>
+    /// Configura a inicialização da aplicação, incluindo rotas e qual DB.
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,6 +23,10 @@ namespace SalesManager
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Configura os serviços a serem utilizados pela aplicação.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
@@ -35,8 +38,15 @@ namespace SalesManager
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            //Método Antigo, que utiliza o SQL Server.
+            //services.AddDbContext<SalesManagerContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("SalesManagerContext")));
+
+            //Adicionar ao projeto o EntityFramework na versão 2.1.1 que é a mesma do .Net Core.
+            //Devo utilizar o nome da classe criada na pasta "Data".
             services.AddDbContext<SalesManagerContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesManagerContext")));
+                                                        options.UseMySql(Configuration.GetConnectionString("SalesManagerContext"), builder =>
+                                                        builder.MigrationsAssembly("SalesManager")));//Deixar o nome da solução
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +58,7 @@ namespace SalesManager
             }
             else
             {
+                //Em caso de erro, ele irá direcionar para a seguinte página abaixo.
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
@@ -56,6 +67,7 @@ namespace SalesManager
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            //Aponta a rota padrão que a aplicação irá seguir.
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
