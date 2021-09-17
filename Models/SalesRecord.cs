0c21a29ddcbc34e3d4838e27064dc1a2295e9828
Marco Aurélio Pavini Mondini <marco-aurelio.pm@hotmail.com>
2021-09-16 21:35:00 -0300

    Criadas as classes Seller, SaleStatus, Salesrecord.

---
+++ Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SalesManager.Models.Enum;
+
+namespace SalesManager.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        /// <summary>
+        /// Salve status Pending = 0, Billed = 1, Canceled = 2
+        /// </summary>
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ Models/Seller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesManager.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email{ get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Basesalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double basesalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Basesalary = basesalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        /// <summary>
+        /// Calculate total sales.
+        /// </summary>
+        /// <param name="initial">Initial Date</param>
+        /// <param name="final">Final Date</param>
+        /// <returns>Total Sales</returns>
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
