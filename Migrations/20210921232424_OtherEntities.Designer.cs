e6c42eb073dacb3abd6d27ab43e73f4159f21b3c
Marco Aurélio Pavini Mondini <marco-aurelio.pm@hotmail.com>
2021-09-21 20:28:55 -0300

    Outras classes e segunda migration.

--- Migrations/20210921232424_OtherEntities.Designer.cs
+++ Migrations/20210921232424_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesManager.Data;
 
 namespace SalesManager.Migrations
 {
     [DbContext(typeof(SalesManagerContext))]
-    partial class SalesManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20210921232424_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
