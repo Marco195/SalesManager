1cececd24e89fe930395932d6af969cce86ff468
Marco Aurélio Pavini Mondini <marco-aurelio.pm@hotmail.com>
2021-09-21 21:19:00 -0300

    Adicionado o Sellers controller.

--- Startup.cs
+++ Startup.cs
@@ -54,6 +54,9 @@ namespace SalesManager
 
         }
 
+        /*Para poder adicionar um objeto e configurar a aplicação no método Configure é necessário registrar a injeção de depenência da classe
+         utilizando services.AddScoped<NomeDaClase>()*/
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService) 
